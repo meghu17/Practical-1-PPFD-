c32849a9a241e5668185ced79dbd6423fe98e5b5
meghana <138484907+meghu17@users.noreply.github.com>
2024-01-22 12:37:50 +0530

    Add files via upload

---
+++ Practical -1.py
@@ -0,0 +1,3 @@
+C = int(input("Enter the degree-c: "))
+F = 9 * C / 5 + 32
+print(F, 'f')
---
+++ pratcaial 1(2).py
@@ -0,0 +1,3 @@
+F = int(input("Enter the Fahrenheit temperature: "))
+C = (F - 32) * 5/9
+print(C, 'Celsius')
